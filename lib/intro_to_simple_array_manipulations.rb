addb65476829f8b0c1ee1958f86e52b2984f9eea
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 23:25:46 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,60 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  colors_in_the_rainbow.push("violet") = updated_array
+end 
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift("Staten Island") = @new_neighborhood
+end 
+  
+def using_pop(array)
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  @continents.pop = deleted_string 
+end
+
+def pop_with_args(array)
+  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  @dog_breeds.pop(2,3) = @small_dogs 
+end
+  
+def using_shift(array)
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @my_favorite_cities.shift = @im_so_over_this_city 
+end
+
+def shift_with_args(array)
+  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  @ice_cream_brands.shift(0,1) = @brands_removed
+end
+
+def using_concat(array1, array2)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  @all_my_favs = @my_favorite_things.concat(more_faves)
+end
+
+def using_insert(array, element) 
+  @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  @new_array = @list_of_programming_languages.insert(4, "Python")
+end
+
+def using_uniq(array)
+   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+   new_array = haircuts.uniq
+end
+
+def using_flatten(array)
+  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+  flat_array = instruments.flatten
+end
+
+def using_delete(array, string) 
+   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+   no_offense_steven = instructors.delete("Steven") 
+end 
+
+def using_delete_at(array, integer)
+  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  deleted_robot = famous_robots.delete_at(2) 
+end
