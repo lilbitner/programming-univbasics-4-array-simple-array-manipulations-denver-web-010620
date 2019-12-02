f21d0aafb4b6a62ceb712713f4b9ce522d700b1d
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 23:11:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ end
 def using_concat(array1, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  
+  @all_my_favs = @my_favorite_things 
   
 describe "using_concat" do 
   before(:each) do 
