6906076f16198e729ebf9f9dee7a9dfa677ef237
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 23:25:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ end
 def using_concat(array1, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  @all_my_favs = concat(@my_favorite_things, more_faves)
+  @all_my_favs = @my_favorite_things.concat(more_faves)
 end
 
 def using_insert(array, element) 
