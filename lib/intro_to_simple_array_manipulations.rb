6da3cf3059d865075f57c7f01e59872bb0946223
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 23:21:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,48 +36,29 @@ end
 
 def using_insert(array, element) 
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  @new_array = @list_of_programming_languages.insert
-
-describe "using_insert" do 
-  before(:each) do 
-    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    @another_language = "Python"
-    @new_array = using_insert(@list_of_programming_languages, @another_language)
-  end
-
-  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_language)
-  end
-
-  it "increases the length of the array" do 
-    expect(@new_array.length).to eq(8)
-  end
+  @new_array = @list_of_programming_languages.insert(4, "Python")
 end
 
-describe "using_uniq" do 
-  it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
-    haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-    new_array = using_uniq(haircuts)
-    expect(new_array).to eq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"])
-  end
+def using_uniq(array)
+   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+   new_array = haircuts.uniq
 end
 
-describe "using_flatten" do 
-  it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
-     instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-     flat_array = using_flatten(instruments)
-     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
-  end
+def using_flatten(array)
+  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+  flat_array = instruments.flatten
 end
 
+def using_delete(array, string) 
+   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+   no_offense_steven = instructors.delete("Steven") 
+end 
 
-describe "using_delete" do 
-  it "takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string" do
-    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = using_delete(instructors, "Steven")
-    expect(instructors).not_to include("Steven")
-  end
+def using_delete_at(array, integer)
+  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  deleted_robot = famous_robots.delete_at[2] 
 end
+
 
 describe "using_delete_at" do 
   it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
